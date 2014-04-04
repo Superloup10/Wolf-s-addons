f06aa3ca807aebb61dcf25c6c6146d68dba016e9
Superloup10 <Superloup10@gmail.com>
2014-04-05 01:07:04 +0200

    add effect damage zombie and skeleton

--- common/wolf_addons/common/item/kit/silver/SilverSword.java
+++ common/wolf_addons/common/item/kit/silver/SilverSword.java
@@ -1,7 +1,12 @@
 package wolf_addons.common.item.kit.silver;
 
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.EntitySkeleton;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.util.DamageSource;
 import wolf_addons.common.item.WolfItemList;
 
 public class SilverSword extends ItemSword
@@ -19,5 +24,15 @@ public class SilverSword extends ItemSword
 			return true;
 		}
 		return false;
+	}
+	
+	@Override
+	public boolean hitEntity(ItemStack stack, EntityLivingBase attackedLiving, EntityLivingBase attackerLiving)
+	{
+		if(attackedLiving instanceof EntityZombie || attackedLiving instanceof EntitySkeleton)
+		{
+			attackedLiving.attackEntityFrom(DamageSource.generic, 10.0F);//Ne prends pas en compte le +4 rajouté à l'épée.
+		}
+		return super.hitEntity(stack, attackedLiving, attackerLiving);
 	}
 }
